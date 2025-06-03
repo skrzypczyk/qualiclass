0c655e7e5e841cd6a6745243518e1ebc357e063a
Yves Skrzypczyk <ysk@wizards-technologies.com>
2025-06-03 08:23:38 +0200

    add cookibot remove tarteaucitron

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import './tarteaucitron.init.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,6 +7,4 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
